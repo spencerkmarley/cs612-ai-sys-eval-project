c3e248707c7e578fe2b3110ec04790e703e34387
Gerry Chng <gerry.chng@gmail.com>
2022-10-14 18:15:54 +0800

    included L-infinity norm

--- model_tests/util.py
+++ model_tests/util.py
@@ -14,16 +14,20 @@ def l2_norm(x, y):
     res = torch.sum((x - y) ** 2)
     return torch.sqrt(res)
 
+def linf_norm(x, y):
+    """ Compute the L-inf norm between two tensors """
+    res = torch.max(torch.abs(x - y))
+    return res
 
 def main():
     torch.manual_seed(42)
     x = torch.randn(2,2)
     y = torch.randn(2,2)
-    a = l2_norm(x,y)
+    a = linf_norm(x,y)
     
     print(f'Tensor x:\n{x}\n')
     print(f'Tensor y:\n{y}\n')
-    print(f'L2 norm: {a}')
+    print(f'L-inf norm: {a}')
     pass
 
 if __name__ == '__main__':
