c64a9b2bdf54ee04e796c4266a2539b0d1a3ae3e
Gerry Chng <gerry.chng@gmail.com>
2022-10-22 22:04:14 +0800

    Custom perturbed model architectures

--- models/definitions/__init__.py
+++ models/definitions/__init__.py
@@ -3,6 +3,12 @@ import sys
 sys.path.append('../..')
 from model_tests.util import *
 
+# Standard architectures
 from .MNISTNet import MNISTNet
-from .CIFAR10Net import CIFAR10Net, CIFAR10_Noise_Net
+from .CIFAR10Net import CIFAR10Net
 from .CIFAR100Net import CIFAR100Net
+
+# Perturbed architectures
+from .MNISTNet import MNIST_Noise_Net, MNISTNet_NeuronsOff, MNISTNet_AT
+from .CIFAR10Net import CIFAR10_Noise_Net, CIFAR10Net_NeuronsOff, CIFAR10Net_AT
+from .CIFAR100Net import CIFAR100_Noise_Net, CIFAR100Net_NeuronsOff, CIFAR100Net_AT
