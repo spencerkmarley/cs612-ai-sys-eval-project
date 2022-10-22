5884f7a20c20a5e0590c7ea0d2aab93a098f7638
tituslhy <tituslhy@gmail.com>
2022-10-22 21:43:44 +0800

    Updated with all architectuers

--- models/definitions/MNISTNet.py
+++ models/definitions/MNISTNet.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+from model_tests.utils import *
 
 class MNISTNet(nn.Module):
     def __init__(self):
@@ -21,3 +22,71 @@ class MNISTNet(nn.Module):
         x = self.fc4(x)
         output = x # cross entropy in pytorch already includes softmax
         return output
+
+class MNIST_Noise_Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, 10)
+        self.fc4 = nn.Linear(10, 10)
+        self.noise_fc1 = torch.randn(self.fc1.weight.size())*0.01
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        add_noise(self.fc1.weight, self.noise_conv1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        x = F.relu(x)
+        x = self.fc4(x)
+        output = x # cross entropy in pytorch already includes softmax
+        return output
+
+class MNISTNet_NeuronsOff(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, 10)
+        self.fc4 = nn.Linear(10, 10)
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc3(x)
+        x = self.dropout(x)
+        x = F.relu(x)
+        x = self.fc4(x)
+        output = x # cross entropy in pytorch already includes softmax
+        return output
+
+class MNISTNet_AT(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, 10)
+        self.fc4 = nn.Linear(10, 10)
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        activation1 = x
+        x = self.fc3(x)
+        x = F.relu(x)
+        activation2 = x
+        x = self.fc4(x)
+        output = x 
+        return activation1, activation2, output
