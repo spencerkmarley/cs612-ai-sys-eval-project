67f6b8328a5d9358541ee7a132d51384bdf688a2
tituslhy <tituslhy@gmail.com>
2022-10-22 21:37:24 +0800

    Updated with all architectures

--- models/definitions/CIFAR100Net.py
+++ models/definitions/CIFAR100Net.py
@@ -1,4 +1,6 @@
+import torch
 from torch import nn
+from model_tests.util import *
 
 def conv_block(in_channels, out_channels, pool=False):
     layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
@@ -6,7 +8,6 @@ def conv_block(in_channels, out_channels, pool=False):
               nn.ReLU(inplace=True)]
     if pool: layers.append(nn.MaxPool2d(2))
     return nn.Sequential(*layers)
-
 
 class CIFAR100Net(nn.Module):
     # from https://medium.com/@alitbk/image-classification-in-a-nutshell-5-different-modelling-approaches-in-pytorch-with-cifar100-8f690866b373
@@ -34,3 +35,97 @@ class CIFAR100Net(nn.Module):
         output = self.res2(output) + output
         output = self.classifier(output)
         return output
+
+class CIFAR100_Noise_Net(nn.Module):
+    # from https://medium.com/@alitbk/image-classification-in-a-nutshell-5-different-modelling-approaches-in-pytorch-with-cifar100-8f690866b373
+    def __init__(self):
+        super().__init__()
+        
+        self.conv1 = conv_block(3, 64)
+        self.conv2 = conv_block(64, 128, pool=True)
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+        
+        self.conv3 = conv_block(128, 256, pool=True)
+        self.conv4 = conv_block(256, 512, pool=True)
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(4), 
+                                        nn.Flatten(), 
+                                        nn.Linear(512, 100))
+        
+        self.noise_conv1 = torch.randn(self.conv1.weight.size())*0.01
+        
+    def forward(self, x):
+        add_noise(self.conv1.weight, self.noise_conv1)
+        output = self.conv1(x)
+        output = self.conv2(output)
+        output = self.res1(output) + output
+        output = self.conv3(output)
+        output = self.conv4(output)
+        output = self.res2(output) + output
+        output = self.classifier(output)
+        return output
+    
+class CIFAR100Net_NeuronsOff(nn.Module):
+    # from https://medium.com/@alitbk/image-classification-in-a-nutshell-5-different-modelling-approaches-in-pytorch-with-cifar100-8f690866b373
+    def __init__(self):
+        super().__init__()
+        
+        self.conv1 = conv_block(3, 64)
+        self.conv2 = conv_block(64, 128, pool=True)
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+        
+        self.conv3 = conv_block(128, 256, pool=True)
+        self.conv4 = conv_block(256, 512, pool=True)
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(4), 
+                                        nn.Flatten(), 
+                                        nn.Linear(512, 100))
+        
+        self.dropout = nn.Dropout(0.1)
+        
+    def forward(self, x):
+        output = self.conv1(x)
+        output = self.dropout(output)
+        
+        output = self.conv2(output)
+        output = self.res1(output) + output
+        output = self.dropout(output)
+        
+        output = self.conv3(output)
+        output = self.conv4(output)
+        output = self.res2(output) + output
+        output = self.dropout(output)
+        
+        output = self.classifier(output)
+        return output
+
+class CIFAR100Net_AT(nn.Module):
+    # from https://medium.com/@alitbk/image-classification-in-a-nutshell-5-different-modelling-approaches-in-pytorch-with-cifar100-8f690866b373
+    def __init__(self):
+        super().__init__()
+        
+        self.conv1 = conv_block(3, 64)
+        self.conv2 = conv_block(64, 128, pool=True)
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+        
+        self.conv3 = conv_block(128, 256, pool=True)
+        self.conv4 = conv_block(256, 512, pool=True)
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(4), 
+                                        nn.Flatten(), 
+                                        nn.Linear(512, 100))
+        
+    def forward(self, x):
+        output = self.conv1(x)
+        output = self.conv2(output)
+        output = self.res1(output) + output
+        output = self.conv3(output)
+        output = self.conv4(output)
+        activation1 = output
+        output = self.res2(output) + output
+        activation2 = output
+        output = self.classifier(output)
+        return activation1, activation2, output
