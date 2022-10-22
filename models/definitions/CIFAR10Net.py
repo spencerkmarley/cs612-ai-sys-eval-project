78a79219e7a5e4ee7dee088404b3f3740cb8cbdb
Gerry Chng <gerry.chng@gmail.com>
2022-10-22 21:17:48 +0800

    included CIFAR10_Noise_Net definition

--- models/definitions/CIFAR10Net.py
+++ models/definitions/CIFAR10Net.py
@@ -49,3 +49,63 @@ class CIFAR10Net(nn.Module):
 
         output = x
         return output
+
+
+### Definition for perturbations to the model
+class CIFAR10_Noise_Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.pool1 = nn.MaxPool2d(2, 2) # output: 64 x 16 x 16
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(2, 2) # output: 128 x 8 x 8
+
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.pool3 = nn.MaxPool2d(2, 2) # output: 256 x 4 x 4
+
+        self.fc1 = nn.Linear(256 * 4 * 4, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 10)
+
+        #New layer - noising. Multiply by 0.01 so that performance doesn't
+        #degrade too much
+        self.noise_conv1 = torch.randn(self.conv1.weight.size())*0.01
+        
+    def forward(self, x):
+        add_noise(self.conv1.weight, self.noise_conv1)
+
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+
+        x = self.conv5(x)
+        x = F.relu(x)
+        x = self.conv6(x)
+        x = F.relu(x)
+        x = self.pool3(x)
+
+        x = torch.flatten(x, 1)        
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+
+        output = x
+        return output
+
+def add_noise(weights, noise, device = device):                                                                                                                                                                                                                                              
+    with torch.no_grad():                                                                                                                                                                                                                                                  
+        weights.add_(noise.to(device))      
