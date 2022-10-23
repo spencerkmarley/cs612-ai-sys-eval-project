31ed87ab78d4515252017370b0ce1219ab111706
Spencer Marley <spencermarley@gmail.com>
2022-10-23 20:56:56 +0800

    Updates to main.py and test_retrain_weights

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 
 # Import custom libraries
 from model_tests import robustness as rb
+from model_tests import test_retrain_weights as rt
 from models.train import train_mnist
 
 # Load model definitions
@@ -24,36 +25,42 @@ import models
 from models.definitions import MNISTNet, CIFAR10Net, CIFAR100Net
 
 # Provide filepaths
-benign_model_file_path = "models/benign/mnist.pt"
-subject_model_file_path = "models/subject/mnist_backdoored_1.pt"
 data_file_path = "data/"
+
+# Provide test cases
+TEST_CASE = 1
+
+if TEST_CASE == 1:
+    network_definition = MNISTNet()
+    benign_model_file_path = "models/benign/mnist.pt"
+    subject_model_file_path = "models/subject/mnist_backdoored_1.pt"
+    dataset = datasets.MNIST(data_file_path, train=False, transform=transforms.ToTensor())
+    
+elif TEST_CASE == 2:
+    network_definition = CIFAR10Net()
+    benign_model_file_path = "models/benign/benign_CIFAR10.pt"
+    subject_model_file_path = "./models/subject/best_model_CIFAR10_10BD.pt"
 
 # Set parameters
 NUM_IMG = 10
 EPS = 0.2
 THRESHOLD = 0.3
 
-# Import benign model(s)
-benign_model = MNISTNet()
+# Import benign model
+benign_model = network_definition
 benign_model.load_state_dict(torch.load(benign_model_file_path))
 
-# Import subject model(s)
-subject_model = MNISTNet()
+# Import subject model
+subject_model = network_definition
 subject_model.load_state_dict(torch.load(subject_model_file_path))
 
-# Import dataset(s)
-mnist_dataset = datasets.MNIST(data_file_path, train=False, transform=transforms.ToTensor())
-
 # Retrain the subject model and test the weights to deteremine if there is a back door
-# test_retrain_weights.py
-
-temp = 11
-rb.test_robust(benign=benign_model, subject=subject_model, dataset=mnist_dataset, test=temp, num_img=NUM_IMG, eps=EPS, threshold=THRESHOLD, verbose=False)
+# rt.main(network=network_definition, subject=subject_model, trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms.ToTensor()), testset = datasets.CIFAR10(root='./data', train=False,download=True, transform=transforms.ToTensor()), retrained="./models/retrained/retrained_CIFAR10_10BD_", n_control_models = 2, verbose=True)
 
 # Test robustness of model using robustness.py tests to determine if there is a backdoor
 for i in range(13):
     try:
-        robust = rb.test_robust(benign=benign_model, subject=subject_model, dataset=mnist_dataset, test=i, num_img=NUM_IMG, eps=EPS, threshold=THRESHOLD, verbose=False)
+        robust = rb.test_robust(benign=benign_model, subject=subject_model, dataset=dataset, test=i, num_img=NUM_IMG, eps=EPS, threshold=THRESHOLD, verbose=False)
         print(robust)
     except:
         print("Robustness test " + str(i) + " failed")
--- model_tests/test_retrain_weights.py
+++ model_tests/test_retrain_weights.py
@@ -24,57 +24,6 @@ import os
 import pathlib
 
 # import util
-
-"""Clean model"""
-
-class CIFAR10Net(nn.Module):
-    # from https://www.kaggle.com/code/shadabhussain/cifar-10-cnn-using-pytorch
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.pool1 = nn.MaxPool2d(2, 2) # output: 64 x 16 x 16
-
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.pool2 = nn.MaxPool2d(2, 2) # output: 128 x 8 x 8
-
-        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
-        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.pool3 = nn.MaxPool2d(2, 2) # output: 256 x 4 x 4
-
-        self.fc1 = nn.Linear(256 * 4 * 4, 1024)
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 10)
-        
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = self.pool1(x)
-
-        x = self.conv3(x)
-        x = F.relu(x)
-        x = self.conv4(x)
-        x = F.relu(x)
-        x = self.pool2(x)
-
-        x = self.conv5(x)
-        x = F.relu(x)
-        x = self.conv6(x)
-        x = F.relu(x)
-        x = self.pool3(x)
-
-        x = torch.flatten(x, 1)        
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        x = F.relu(x)
-        x = self.fc3(x)
-
-        output = x
-        return output
     
 # FUNCTION DEFINITIONS
 def load_model(model_class, name, device):
@@ -98,7 +47,7 @@ def get_weights_from_model(model):
             weights[name] = param.data
     return weights
 
-def train(model, dataloader, loss_fn, optimizer, device):
+def train(model, dataloader, loss_fn, optimizer, device, verbose=False):
     size = len(dataloader.dataset)
     model.train()
 
@@ -116,9 +65,10 @@ def train(model, dataloader, loss_fn, optimizer, device):
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(x)
-            print('loss: {:.4f} [{}/{}]'.format(loss, current, size))
+            if verbose:
+                print('loss: {:.4f} [{}/{}]'.format(loss, current, size))
             
-def test(model, dataloader, loss_fn, device):
+def test(model, dataloader, loss_fn, device, verbose=False):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.to(device)
@@ -136,12 +86,12 @@ def test(model, dataloader, loss_fn, device):
     loss /= num_batches
     correct /= size
     accuracy = 100*correct
-    print('Test Result: Accuracy @ {:.2f}%, Avg loss @ {:.4f}\n'.format(accuracy, loss))
+    if verbose:
+        print('Test Result: Accuracy @ {:.2f}%, Avg loss @ {:.4f}\n'.format(accuracy, loss))
 
     return accuracy, loss
 
-
-def main():
+def main(network, subject, trainset, testset, retrained, n_control_models = 2, verbose=False):
     """Load subject model and get subject model's summary and weights"""
 
     # Device selection - includes Apple Silicon
@@ -153,24 +103,20 @@ def main():
         device = 'cpu'
 
     # Load the model to be tested for the presence of a potential backdoor
-    subject_model = load_model(CIFAR10Net, './models/best_model_CIFAR10_10BD.pt', device)
+    subject_model = load_model(network, subject, device)
     subject_model_weights = get_weights_from_model(subject_model)
     subject_fc3_weights = subject_model_weights['fc3.weight'][0]
     subject_fc3_bias = subject_model_weights['fc3.bias'][0]
 
     """Retrain subject model"""
-    transform = transforms.ToTensor()
-
     train_kwargs = {'batch_size': 100, 'shuffle':True}
     test_kwargs = {'batch_size': 1000}
-    trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    testset = datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
     train_loader = torch.utils.data.DataLoader(trainset, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(testset, **test_kwargs)
 
     """Testing subject model against clean test data"""
 
-    subject_test_accuracy, subject_test_loss = test(subject_model,test_loader,nn.CrossEntropyLoss(),device)
+    subject_test_accuracy, subject_test_loss = test(subject_model,test_loader,nn.CrossEntropyLoss(),device,verbose)
 
     """Retraining subject model for testing"""
     
@@ -179,31 +125,34 @@ def main():
     retrain_models = []
     for n in range(n_control_models):
         FORCE_RETRAIN = True # Only set to True if you want to retrain the model
-        if not os.path.exists('./models/retrained_CIFAR10_10BD_'+str(n)+'.pt') or FORCE_RETRAIN:
-            print(f'Training #{n+1} of {n_control_models} models')
-            retrain_model = CIFAR10Net().to(device)
+        if not os.path.exists(retrained+str(n)+'.pt') or FORCE_RETRAIN:
+            if verbose:
+                print(f'Training #{n+1} of {n_control_models} models')
+            retrain_model = network.to(device)
             optimizer = optim.Adam(retrain_model.parameters(), lr=0.001)
             epochs = 30
             best_accuracy = 0
 
             for epoch in range(epochs):
-                print('\n------------- Epoch {} -------------\n'.format(epoch+1))
-                train(retrain_model, train_loader, nn.CrossEntropyLoss(), optimizer, device)
-                accuracy, loss = test(retrain_model, test_loader, nn.CrossEntropyLoss(), device)
+                if verbose:
+                    print('\n------------- Epoch {} -------------\n'.format(epoch+1))
+                train(retrain_model, train_loader, nn.CrossEntropyLoss(), optimizer, device, verbose)
+                accuracy, loss = test(retrain_model, test_loader, nn.CrossEntropyLoss(), device, verbose)
 
                 #Callback to save model with lowest loss
                 if accuracy > best_accuracy:
-                    print(f'Saving model with new best accuracy: {accuracy:.4f}')
-                    save_model(retrain_model,'./models/retrained_CIFAR10_10BD_'+str(n)+'.pt')
+                    if verbose:
+                        print(f'Saving model with new best accuracy: {accuracy:.4f}')
+                    save_model(retrain_model, retrained+str(n)+'.pt')
                     best_accuracy = accuracy
         
         # Regardless of whether we retrained the model, load it so we have the best model saved
-        retrain_model = load_model(CIFAR10Net, './models/retrained_CIFAR10_10BD_'+str(n)+'.pt',device=device)
+        retrain_model = load_model(network, retrained+str(n)+'.pt',device=device)
         retrain_models.append(retrain_model)
 
     """Compare weights and biases of the feature vector layer."""
     retrain_model = retrain_models[0]
-    test(retrain_model,test_loader,nn.CrossEntropyLoss(),device)
+    test(retrain_model,test_loader,nn.CrossEntropyLoss(),device, verbose)
     
     # TEST DIFFERENCES BETWEEN RETRAINED MODELS
     subject_model = retrain_models[1]
@@ -224,25 +173,27 @@ def main():
     maxbound = q75+1.5*iqr
     minbound = q25-1.5*iqr
 
-    plt.figure(figsize=(20,5))
-    plt.plot(np.arange(1,513),Weight_delta.to('cpu').numpy(),'^--k')
-    plt.axhline(maxbound,0,512)
-    plt.axhline(minbound,0,512)
-    plt.ylabel('Retrain-subject weight delta at last linear layer')
-    plt.xlabel('Neuron number')
-    plt.show()
+    # plt.figure(figsize=(20,5))
+    # plt.plot(np.arange(1,513),Weight_delta.to('cpu').numpy(),'^--k')
+    # plt.axhline(maxbound,0,512)
+    # plt.axhline(minbound,0,512)
+    # plt.ylabel('Retrain-subject weight delta at last linear layer')
+    # plt.xlabel('Neuron number')
+    # plt.show()
 
     num_outlier_neurons = sum(Weight_delta>maxbound)+sum(Weight_delta<minbound)
     percent_outlier_neurons = num_outlier_neurons/len(Weight_delta)
-    print(f'Number of outlier neurons: {num_outlier_neurons.item()}')
-    print(f'Percentage of outlier neurons: {percent_outlier_neurons.item()*100:.2f}%')
+    if verbose:
+        print(f'Number of outlier neurons: {num_outlier_neurons.item()}')
+        print(f'Percentage of outlier neurons: {percent_outlier_neurons.item()*100:.2f}%')
 
     threshold = 0.10 #can be adjusted.
 
-    if percent_outlier_neurons > threshold:
-        print(f'It is possible that the network has a backdoor, becuase the percentage of outlier neurons is above the {threshold} threshold.')
-    else:
-        print('It is unlikely that the network has a backdoor.')
-        
+    if verbose:
+        if percent_outlier_neurons > threshold:
+            print(f'It is possible that the network has a backdoor, becuase the percentage of outlier neurons is above the {threshold} threshold.')
+        else:
+            print('It is unlikely that the network has a backdoor.')
+            
 if __name__ == '__main__':
     main()
