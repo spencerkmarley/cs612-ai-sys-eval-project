9fe3560f3f318a5e9b06c39bcea1abcb2a883b80
tituslhy <tituslhy@gmail.com>
2022-10-23 22:44:55 +0800

    Update robustness.py

--- model_tests/robustness.py
+++ model_tests/robustness.py
@@ -41,31 +41,31 @@ def test_robust(benign, subject, dataset, test, num_img, eps, threshold, mnist, 
     robust = True
 
     if test == 0:
-        perturb_rotation(benign, subject, dataset, num_img, threshold, verbose=False)
+        perturb_rotation(benign, subject, dataset, num_img, threshold, verbose=verbose)
     elif test == 1:
-        perturb_change_pixels(benign, subject, dataset, test, num_img, eps, threshold, verbose=False)
+        perturb_change_pixels(benign, subject, dataset, test, num_img, eps, threshold, verbose=verbose)
     elif test == 2:
-        perturb_invert(benign, subject, dataset, num_img, threshold, verbose=False)
+        perturb_invert(benign, subject, dataset, num_img, threshold, verbose=verbose)
     elif test == 3:
-        perturb_change_lighting(benign, subject, dataset, num_img, threshold, verbose=False)
+        perturb_change_lighting(benign, subject, dataset, num_img, threshold, verbose=verbose)
     elif test == 4:
-        perturb_zoom_in_out(benign, subject, dataset, num_img, threshold, verbose=False)
+        perturb_zoom_in_out(benign, subject, dataset, num_img, threshold, verbose=verbose)
     elif test == 5:
-        perturb_resize(benign, subject, dataset, test, num_img, eps, threshold, verbose=False)
+        perturb_resize(benign, subject, dataset, test, num_img, eps, threshold, verbose=verbose)
     elif test == 6:
-        perturb_crop_rescale(benign, subject, dataset, num_img, threshold, verbose=False)
+        perturb_crop_rescale(benign, subject, dataset, num_img, threshold, verbose=verbose)
     elif test == 7:
-        perturb_bit_depth_reduction(benign, subject, dataset, num_img, threshold, verbose=False)
+        perturb_bit_depth_reduction(benign, subject, dataset, num_img, threshold, verbose=verbose)
     elif test == 8:
-        perturb_compress_decompress(benign, subject, dataset, test, num_img, eps, threshold, verbose=False)
+        perturb_compress_decompress(benign, subject, dataset, test, num_img, eps, threshold, verbose=verbose)
     elif test == 9:
-        perturb_total_var_min(benign, subject, dataset, test, num_img, eps, threshold, verbose=False)
+        perturb_total_var_min(benign, subject, dataset, test, num_img, eps, threshold, verbose=verbose)
     elif test == 10:
-        perturb_adding_noise(benign, subject, dataset, num_img, threshold, verbose=False)
+        perturb_adding_noise(benign, subject, dataset, num_img, threshold, verbose=verbose)
     elif test == 11:
-        perturb_watermark(benign, subject, dataset, num_img, threshold, MNIST= mnist, verbose=False)
+        perturb_watermark(benign, subject, dataset, num_img, threshold, MNIST= mnist, verbose=verbose)
     elif test == 12:
-        perturb_whitesquare(benign, subject, dataset, num_img, threshold, MNIST= mnist, verbose=False)
+        perturb_whitesquare(benign, subject, dataset, num_img, threshold, MNIST= mnist, verbose=verbose)
     else:
         print("Please provide a valid test number")
     
