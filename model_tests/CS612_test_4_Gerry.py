7db1647094a51b089c24cefcb923a455827d3740
Gerry Chng <gerry.chng@gmail.com>
2022-10-23 18:07:08 +0800

    interim progress - streamline to py

--- model_tests/CS612_test_4_Gerry.py
+++ model_tests/CS612_test_4_Gerry.py
@@ -36,7 +36,7 @@ import util
 from util import get_pytorch_device, open_model, load_model, save_model, train, test
 
 # Import models with perturbations
-from models.definitions import CIFAR10_Noise_Net
+from models.definitions import CIFAR10_Noise_Net, CIFAR10Net_NeuronsOff
 
 
 #
@@ -80,7 +80,7 @@ def prediction_variance(subject_model, test_model):
 
 # Load the subject model
 from models.definitions import CIFAR10Net
-subject_model_filename = 'models/subject/cifar10_backdoored_1.pt'
+subject_model_filename = 'models/subject/best_model_CIFAR10_10BD.pt'
 subject_model = load_model(CIFAR10Net, subject_model_filename)
 
 #subject_model = open_model(subject_model_filename)
@@ -110,30 +110,95 @@ pred_distribution = util.model.get_pred_distribution(subject_model, test_loader,
 print(f' Class distribution from inference of clean model: {pred_distribution}')
 
 
+def retrain_model(
+  base_model_filename,  # Location of the base PyTorch model to be loaded from file
+  retrain_arch,  # Model architecture to retrain - found in models/definitions
+  save_filename,  # Location to save the retrained model
+  train_loader,  # Training data loader
+  device = None,  # Device to use for training (if None, use get_pytorch_device())
+  epochs = 30,  # Training epochs
+  lr = 0.001,  # Learning rate
+  force_retrain = False,  # Force retrain even if model file exists
+  ):
+  """ Retrains a model from the base with alterations from the retrain_arch """
+  
+  if device is None:
+    device = get_pytorch_device()
+  
+  if force_retrain or not os.path.exists(save_filename):
+    new_model = load_model(retrain_arch, base_model_filename)
+    optimizer = optim.Adam(new_model.parameters(), lr=lr)
+    best_accuracy = 0
+    
+    for epoch in range(epochs):
+      print('\n------------- Epoch {} -------------\n'.format(epoch+1))
+      train(new_model, train_loader, nn.CrossEntropyLoss(), optimizer, device)
+      accuracy, _ = test(new_model, test_loader, nn.CrossEntropyLoss(), device)
+
+      if accuracy > best_accuracy:
+        save_model(new_model, save_filename)
+        best_accuracy = accuracy
+        
+  new_model = load_model(retrain_arch, save_filename)
+  return new_model
+
+  
 #
 # TEST 1 - Gaussian noise
 #
+save_filename = os.path.join('models', 'CIFAR10NET_noised.pt')
+model_noise = retrain_model(
+  base_model_filename = subject_model_filename,
+  retrain_arch = CIFAR10_Noise_Net,
+  save_filename = save_filename,
+  train_loader = train_loader,
+  force_retrain = False,
+)
+
+# if FORCE_RELOAD or not os.path.exists(save_filename):
+#     model_noise = load_model(CIFAR10_Noise_Net, subject_model_filename)
+#     optimizer = optim.Adam(model_noise.parameters(),lr = 0.001)
+#     epochs = 30
+#     best_accuracy = 0
+
+#     for epoch in range(epochs):
+#         print('\n------------- Epoch {} -------------\n'.format(epoch+1))
+#         train(model_noise, train_loader, nn.CrossEntropyLoss(), optimizer, device)
+#         accuracy, loss = test(model_noise, test_loader, nn.CrossEntropyLoss(), device)
+
+#         if accuracy > best_accuracy:
+#             save_model(model_noise, save_filename)
+#             best_accuracy = accuracy
+    
+# model_noise = load_model(CIFAR10_Noise_Net, save_filename)
+
+backdoored_classes = has_backdoor(subject_model, model_noise)
+if len(backdoored_classes):
+    print('The subject model has a backdoor')
+    print(backdoored_classes)
+
+#
+# TEST 2 - Randomly switch off neurons
+#
 FORCE_RELOAD = False
-save_filename = os.path.join('models', 'noised_CIFAR10NET.pt')
+save_filename = os.path.join('models', 'CIFAR10NET_NeuronsOff.pt')
 
-if FORCE_RELOAD or not os.path.exists(save_filename):
-    model_noise = load_model(CIFAR10_Noise_Net, subject_model_filename)
-    optimizer = optim.Adam(model_noise.parameters(),lr = 0.001)
-    epochs = 30
-    best_accuracy = 0
+model_NeuronsOff = load_model(CIFAR10Net_NeuronsOff, subject_model_filename)
+optimizer = optim.Adam(model_NeuronsOff.parameters(),lr = 0.001)
+epochs =30
+best_accuracy = 0
 
-    for epoch in range(epochs):
-        print('\n------------- Epoch {} -------------\n'.format(epoch+1))
-        train(model_noise, train_loader, nn.CrossEntropyLoss(), optimizer, device)
-        accuracy, loss = test(model_noise, test_loader, nn.CrossEntropyLoss(), device)
+for epoch in range(epochs):
+  print('\n------------- Epoch {} -------------\n'.format(epoch+1))
+  train(model_NeuronsOff, train_loader, nn.CrossEntropyLoss(), optimizer, device)
+  accuracy, loss, output = test(model_NeuronsOff, test_loader, nn.CrossEntropyLoss(), device)
 
-        if accuracy > best_accuracy:
-            save_model(model_noise, save_filename)
-            best_accuracy = accuracy
+  # Callback to save model with lowest loss
+  if accuracy > best_accuracy:
+    save_model(model_NeuronsOff, save_filename)
+    best_accuracy = accuracy
     
-    
-model_noise = load_model(CIFAR10_Noise_Net, './models/noised_CIFAR10NET.pt')
-
+model_NeuronsOff = load_model(CIFAR10Net_NeuronsOff, save_filename)
 backdoored_classes = has_backdoor(subject_model, model_noise)
 if len(backdoored_classes):
     print('The subject model has a backdoor')
