6491ae9da79850db503dc060fcda45ed2f8956e1
Gerry Chng <gerry.chng@gmail.com>
2022-10-31 11:35:37 +0800

    permit forcing CPU

--- util/pytorch_functions.py
+++ util/pytorch_functions.py
@@ -1,9 +1,17 @@
 import torch
 
+FORCE_CPU = False
 def get_pytorch_device():
-    if torch.cuda.is_available():
-        return 'cuda'
-    elif torch.backends.mps.is_available():
-        return 'mps'
-    else:
-        return 'cpu'
+    if not FORCE_CPU:
+        if torch.cuda.is_available():
+            return 'cuda'
+        elif torch.backends.mps.is_available():
+            return 'mps'
+    
+    return 'cpu'
+
+def main():
+    print(get_pytorch_device())
+    
+if __name__ == '__main__':
+    main()
