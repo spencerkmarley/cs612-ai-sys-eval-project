1b2dcdb4178b60aa58625c4ae8f233ae035f9e38
hestella <stellahechen@gmail.com>
2022-11-02 22:01:55 +0800

    Update config.py

--- util/config.py
+++ util/config.py
@@ -11,8 +11,8 @@ SUBJECT_MODEL_FILE_PATH= "models/subject/mnist_backdoored_1.pt" #filepath of upl
 FORCE_RETRAIN = False   
 
 #the percentage of input data used for trigger synthesis on CIFAR100 models, a float in [0.04 , 1]
-#CIFAR100_pct=1:uses the full training data and will run > 7horus on GPU !!!
-#CIFAR100_pct=0.01:uses the 4% of training data and will run ~1h20min for trigger synthesis, 3-4 hours in total
+#CIFAR100_pct=1: use the full training data and will run > 7horus on GPU !!!
+#CIFAR100_pct=0.04: use 4% of training data and will run ~1h20min for trigger synthesis, 3-4 hours in total
 CIFAR100_pct=0.04
 
 # Modelling parameters
